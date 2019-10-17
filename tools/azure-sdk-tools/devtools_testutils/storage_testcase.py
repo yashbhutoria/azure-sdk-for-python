72b1b27d5e4d4809cfcf55fefe4d3ea1d30163e7
Laurent Mazuel <laurent.mazuel@gmail.com>
2019-10-16 17:17:05 -0700

    WIP

--- tools/azure-sdk-tools/devtools_testutils/storage_testcase.py
+++ tools/azure-sdk-tools/devtools_testutils/storage_testcase.py
@@ -7,6 +7,7 @@ from azure_devtools.scenario_tests.preparers import (
     AbstractPreparer,
     SingleValueReplacer,
 )
+from azure_devtools.scenario_tests import create_random_name
 from azure_devtools.scenario_tests.exceptions import AzureTestError
 
 from . import AzureMgmtPreparer, ResourceGroupPreparer, FakeResource
@@ -25,17 +26,20 @@ class StorageAccountPreparer(AzureMgmtPreparer):
                  parameter_name='storage_account',
                  resource_group_parameter_name=RESOURCE_GROUP_PARAM,
                  disable_recording=True, playback_fake_resource=None,
-                 client_kwargs=None):
+                 client_kwargs=None,
+                 random_name_enabled=False):
         super(StorageAccountPreparer, self).__init__(name_prefix, 24,
                                                      disable_recording=disable_recording,
                                                      playback_fake_resource=playback_fake_resource,
-                                                     client_kwargs=client_kwargs)
+                                                     client_kwargs=client_kwargs,
+                                                     random_name_enabled=random_name_enabled)
         self.location = location
         self.sku = sku
         self.kind = kind
         self.resource_group_parameter_name = resource_group_parameter_name
         self.parameter_name = parameter_name
         self.storage_key = ''
+        self.resource_moniker = self.name_prefix + "storname"
 
     def create_resource(self, name, **kwargs):
         if self.is_live:
@@ -56,6 +60,11 @@ class StorageAccountPreparer(AzureMgmtPreparer):
                 for v in self.client.storage_accounts.list_keys(group.name, name).keys
             }
             self.storage_key = storage_keys['key1']
+
+            self.test_class_instance.scrubber.register_name_pair(
+                name,
+                self.resource_moniker
+            )
         else:
             self.resource = FakeResource(name=name, id=name)
             self.storage_key = 'ZmFrZV9hY29jdW50X2tleQ=='
